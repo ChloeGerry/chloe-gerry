39bfc60cf089a9b0984fa9bc4c8ad45d26794281
Chloé Gerry <chloe.gerry@gmail.com>
2023-07-22 17:44:04 +0200

    create a loader and a json file to add dynamically the projects data

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,11 @@
+import { LoaderWrapper, LoaderContent } from './loader.style';
+
+const Loader = () => {
+  return (
+    <LoaderWrapper>
+      <LoaderContent />
+    </LoaderWrapper>
+  );
+};
+
+export default Loader;
